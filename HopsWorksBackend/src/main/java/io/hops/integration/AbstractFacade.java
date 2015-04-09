34aa37c3a7ed447ac4812f2e9a5051b86ebd7084
More <amore@kth.se>
2015-04-09 20:34:46 +0200

    Removed some retard commenting.

--- HopsWorksBackend/src/main/java/io/hops/integration/AbstractFacade.java
+++ HopsWorksBackend/src/main/java/io/hops/integration/AbstractFacade.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package io.hops.integration;
 
 import java.util.List;
@@ -9,7 +5,7 @@ import javax.persistence.EntityManager;
 
 /**
  *
- * @author Owner
+ * @author André & Ermias
  */
 public abstract class AbstractFacade<T> {
     private Class<T> entityClass;
