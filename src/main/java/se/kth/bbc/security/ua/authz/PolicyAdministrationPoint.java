a186039a8f1a261ed953132b295f88af2947d1f0
Ali Gholami <gholami@sics.se>
2015-12-01 16:20:47 +0100

    better error handling

--- src/main/java/se/kth/bbc/security/ua/authz/PolicyAdministrationPoint.java
+++ src/main/java/se/kth/bbc/security/ua/authz/PolicyAdministrationPoint.java
@@ -13,7 +13,6 @@ import se.kth.bbc.security.ua.UserManager;
 import se.kth.bbc.security.ua.YubikeyActivator;
 import se.kth.hopsworks.user.model.Users;
 
-
 public class PolicyAdministrationPoint implements Serializable {
 
   private static final long serialVersionUID = 1L;
@@ -25,58 +24,109 @@ public class PolicyAdministrationPoint implements Serializable {
   private UserManager userPolicMgr;
 
   public boolean isInAdminRole(Users user) {
-    return userPolicMgr.findGroups(user.getUid()).contains(BBCGroup.SYS_ADMIN.
-            name());
+    if (userPolicMgr.findGroups(user.getUid()).contains(BBCGroup.SYS_ADMIN.
+            name())) {
+      return true;
+    }
+    return false;
   }
 
   public boolean isInDataProviderRole(Users user) {
-    return userPolicMgr.findGroups(user.getUid()).contains(BBCGroup.BBC_ADMIN.
-            name());
+    if (userPolicMgr.findGroups(user.getUid()).contains(BBCGroup.BBC_ADMIN.
+            name())) {
+      return true;
+    }
+    return false;
   }
 
   public boolean isInAuditorRole(Users user) {
-    return userPolicMgr.findGroups(user.getUid()).contains(BBCGroup.AUDITOR.
-            name());
+    if (userPolicMgr.findGroups(user.getUid()).contains(BBCGroup.AUDITOR.
+            name())) {
+      return true;
+    }
+    return false;
   }
 
   public boolean isInResearcherRole(Users user) {
-    return userPolicMgr.findGroups(user.getUid()).contains(
-            BBCGroup.BBC_RESEARCHER.name());
+    if (userPolicMgr.findGroups(user.getUid()).contains(BBCGroup.BBC_RESEARCHER.
+            name())) {
+      return true;
+    }
+    return false;
   }
 
   public boolean isInGuestRole(Users user) {
-    return userPolicMgr.findGroups(user.getUid()).contains(BBCGroup.BBC_GUEST.
-            name());
+    if (userPolicMgr.findGroups(user.getUid()).contains(BBCGroup.BBC_GUEST.
+            name())) {
+      return true;
+    }
+    return false;
   }
 
   public boolean isInAdminRole(String username) {
-    Users user = userPolicMgr.getUserByEmail(username);
-    return userPolicMgr.findGroups(user.getUid()).contains(BBCGroup.SYS_ADMIN.
-            name());
+    Users user = userPolicMgr.getUserByUsername(username);
+    if (userPolicMgr.findGroups(user.getUid()) == null) {
+      return false;
+    }
+
+    if (userPolicMgr.findGroups(user.getUid()).contains(BBCGroup.SYS_ADMIN.
+            name())) {
+      return true;
+    }
+    return false;
   }
 
   public boolean isInResearcherRole(String username) {
     Users user = userPolicMgr.getUserByUsername(username);
-    return userPolicMgr.findGroups(user.getUid()).contains(
-            BBCGroup.BBC_RESEARCHER.name());
+    if (userPolicMgr.findGroups(user.getUid()) == null) {
+      return false;
+    }
+
+    if (userPolicMgr.findGroups(user.getUid()).contains(BBCGroup.BBC_RESEARCHER.
+            name())) {
+      return true;
+    }
+    return false;
   }
 
   public boolean isInDataProviderRole(String username) {
     Users user = userPolicMgr.getUserByUsername(username);
-    return userPolicMgr.findGroups(user.getUid()).contains(BBCGroup.BBC_ADMIN.
-            name());
+    if (userPolicMgr.findGroups(user.getUid()) == null) {
+      return false;
+    }
+
+    if (userPolicMgr.findGroups(user.getUid()).contains(BBCGroup.BBC_ADMIN.
+            name())) {
+      return true;
+    }
+    return false;
   }
 
   public boolean isInAuditorRole(String username) {
+
     Users user = userPolicMgr.getUserByUsername(username);
-    return userPolicMgr.findGroups(user.getUid()).contains(BBCGroup.AUDITOR.
-            name());
+    if (userPolicMgr.findGroups(user.getUid()) == null) {
+      return false;
+    }
+
+    if (userPolicMgr.findGroups(user.getUid()).contains(BBCGroup.AUDITOR.
+            name())) {
+      return true;
+    }
+    return false;
   }
 
   public boolean isInGuestRole(String username) {
     Users user = userPolicMgr.getUserByUsername(username);
-    return userPolicMgr.findGroups(user.getUid()).contains(BBCGroup.BBC_GUEST.
-            name());
+    if (userPolicMgr.findGroups(user.getUid()) == null) {
+      return false;
+    }
+
+    if (userPolicMgr.findGroups(user.getUid()).contains(BBCGroup.BBC_GUEST.
+            name())) {
+      return true;
+    }
+    return false;
   }
 
   public String redirectUser(Users user) {
@@ -85,7 +135,7 @@ public class PolicyAdministrationPoint implements Serializable {
       return "adminIndex";
     } else if (isInAuditorRole(user)) {
       return "auditIndex";
-    } else if (isInDataProviderRole(user) || isInResearcherRole(user) ) {
+    } else if (isInDataProviderRole(user) || isInResearcherRole(user)) {
       return "home";
     }
 
