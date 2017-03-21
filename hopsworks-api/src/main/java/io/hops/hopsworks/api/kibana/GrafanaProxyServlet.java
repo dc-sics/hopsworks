34dbeb1a74cc71700fad668522eddcbbed52feb7
Fadi Samisti <fadisamisti@gmail.com>
2017-03-21 12:50:07 +0100

    Update the grafana proxy filter

--- hopsworks-api/src/main/java/io/hops/hopsworks/api/kibana/GrafanaProxyServlet.java
+++ hopsworks-api/src/main/java/io/hops/hopsworks/api/kibana/GrafanaProxyServlet.java
@@ -35,7 +35,7 @@ public class GrafanaProxyServlet extends ProxyServlet {
           throws ServletException, IOException {
     if (servletRequest.getRequestURI().contains("query")) {
       String email = servletRequest.getUserPrincipal().getName();
-      Pattern pattern = Pattern.compile("(application_.*?_.*?)\\.");
+      Pattern pattern = Pattern.compile("(application_\d{13}_.\d{4,})");
       Matcher matcher = pattern.matcher(servletRequest.getQueryString());
       if (matcher.find()) {
         String appId = matcher.group(1);
