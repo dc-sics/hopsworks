5a92edb430e656f4c6b6b819a581e1e4dbe1e93f
Filotas Siskos <siskos.filotas@gmail.com>
2017-06-30 18:21:32 +0200

    Removed unused imports

--- hopsworks-api/src/main/java/io/hops/hopsworks/api/jobs/DeviceService.java
+++ hopsworks-api/src/main/java/io/hops/hopsworks/api/jobs/DeviceService.java
@@ -9,7 +9,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
@@ -22,13 +21,10 @@ import org.json.JSONObject;
 
 import io.hops.hopsworks.api.filter.NoCacheResponse;
 import io.hops.hopsworks.api.filter.AllowedRoles;
-import io.hops.hopsworks.api.util.JsonResponse;
 import io.hops.hopsworks.common.dao.device.DeviceFacade;
 import io.hops.hopsworks.common.dao.kafka.KafkaFacade;
 import io.hops.hopsworks.common.dao.kafka.SchemaDTO;
-import io.hops.hopsworks.common.dao.kafka.TopicDTO;
 import io.hops.hopsworks.common.exception.AppException;
-import org.apache.avro.Schema;
 
 @RequestScoped
 @TransactionAttribute(TransactionAttributeType.NEVER)
