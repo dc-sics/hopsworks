d90075be2b24283395be5f515bee361216854a78
Jim Dowling <jdowling@sics.se>
2017-08-27 00:02:38 +0200

    more cleaning notebooks

--- hopsworks-api/src/main/java/io/hops/hopsworks/api/jupyter/JupyterNotebookCleaner.java
+++ hopsworks-api/src/main/java/io/hops/hopsworks/api/jupyter/JupyterNotebookCleaner.java
@@ -18,8 +18,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.charset.Charset;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.sql.Date;
 import java.util.ArrayList;
 import java.util.List;
