ad4434e360f5dfc2a65998d14c29eb4c83d3fe93
SICS <sics@filotas.sics.se>
2017-09-10 22:13:14 +0200

    Project Device Activation

--- hopsworks-api/src/main/java/io/hops/hopsworks/api/device/DeviceManagementService.java
+++ hopsworks-api/src/main/java/io/hops/hopsworks/api/device/DeviceManagementService.java
@@ -16,7 +16,6 @@ import javax.enterprise.context.RequestScoped;
 import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
-import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
@@ -24,7 +23,6 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 import javax.ws.rs.core.SecurityContext;
-import java.util.UUID;
 import java.util.logging.Logger;
 
 @RequestScoped
