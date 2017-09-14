63c5a19322b9e909c2ba9101ad93401672b973c3
SICS <sics@filotas.sics.se>
2017-09-14 09:38:36 +0200

    Imports fixed

--- hopsworks-api/src/main/java/io/hops/hopsworks/api/device/DeviceService.java
+++ hopsworks-api/src/main/java/io/hops/hopsworks/api/device/DeviceService.java
@@ -1,7 +1,11 @@
 package io.hops.hopsworks.api.device;
 
 import java.text.MessageFormat;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+import java.util.UUID;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.ejb.EJB;
